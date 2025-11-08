47750adc8e9861f8b5d4e766fc5f74491943a885
Legrand <legrandd@buffalo.edu>
2025-11-08 11:47:04 -0500

    began work in main

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import spotipy
+from pip._internal.cli.cmdoptions import python
+import google-api-python-client
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+spotify_id = os.getenv('SPOTIFY_CLIENT_ID')
+spotify_secret = os.getenv('SPOTIFY_CLIENT_SECRET')
+youtube_key = os.getenv('YOUTUBE_API_KEY')
 
